27e7dda32c331a7275375dce2b2221d28d9f0d87
Krishna Thota <kcthota@google.com>
2026-01-15 15:05:30 -0800

    add assist fixes

--- a2a/chat-client/components/ChatMessage.tsx
+++ a2a/chat-client/components/ChatMessage.tsx
@@ -13,8 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import {appConfig} from '@/config';
 import React from 'react';
+import {appConfig} from '@/config';
 import {
   ChatMessage,
   Checkout,
